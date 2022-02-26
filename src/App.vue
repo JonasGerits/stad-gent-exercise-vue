98392f2a178683a23632d2417072f8904d868c1d
jonasgerits <jonas.gerits@aca-it.be>
2022-02-26 16:36:44 +0100

    Add header component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <HeaderComponent/>
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
+import HeaderComponent from './components/HeaderComponent'
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    HeaderComponent
   }
 }
 </script>
@@ -21,6 +23,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
