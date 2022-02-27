1b17bf7de08a3f00969bc1134dff565f639fe2a8
jonasgerits <jonas.gerits@aca-it.be>
2022-02-27 13:53:06 +0100

    Add responsiveness to list items

--- src/components/PlaygroundListItem.vue
+++ src/components/PlaygroundListItem.vue
@@ -1,12 +1,12 @@
 <template>
  <li class="overflow-hidden border-b-2 min-w-fit">
    <div class="flex gap-4 p-3">
-     <div class="w-48 flex items-center">
-       <div class="w-full h-40">
+     <div class="md:w-32 lg:w-48 flex items-center hidden md:block">
+       <div class="w-full md:h-32 lg:h-40 h-40">
          <img src="../assets/playground-stock-photo.png" class="w-full h-full object-cover rounded-lg">
        </div>
      </div>
-     <div class="flex flex-col justify-center items-start w-72">
+     <div class="flex flex-col justify-center items-start w-60">
        <div class="font-bold text-xl text-left ">{{record.fields.naam}}</div>
        <div class="mb-3 text-sm">{{record.size}} m²</div>
        <div class="flex flex-wrap">
